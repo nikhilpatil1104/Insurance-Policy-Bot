2a6c41e3b37770be7777cd42507d0d8f37742c54
Nikhil Patil <129036247+nikhilpatil1104@users.noreply.github.com>
2026-03-24 20:23:49 -0400

    Update import paths for text splitters and prompts

--- app.py
+++ app.py
@@ -18,11 +18,11 @@ st.set_page_config(
 @st.cache_resource(show_spinner=False)
 def _load_libs():
     from langchain_community.document_loaders import PyPDFLoader
-    from langchain_text_splitters import RecursiveCharacterTextSplitter
+    from langchain_text_splitters import RecursiveCharacterTextSplitter    
     from langchain_openai import OpenAIEmbeddings, ChatOpenAI
     from langchain_community.vectorstores import FAISS
     from langchain.chains import RetrievalQA
-    from langchain.prompts import PromptTemplate
+    from langchain_core.prompts import PromptTemplate
     return (
         PyPDFLoader, RecursiveCharacterTextSplitter,
         OpenAIEmbeddings, ChatOpenAI, FAISS, RetrievalQA, PromptTemplate,
